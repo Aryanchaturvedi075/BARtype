c81c5d5b3d49688257a41bcc109a43a6ed44fb9c
Aryan Chaturvedi <aryanchaturvedi075@gmail.com>
2024-12-02 18:47:37 -0400

    Ah shit, here we go again

--- backend/src/tests/setup.js
+++
@@ -1,8 +0,0 @@
-// backend/src/tests/setup.js
-import { TextService } from '../core/typing/TextService';
-import { MetricsService } from '../core/metrics/MetricsService';
-import { EventEmitter } from 'events';
-
-global.TextService = TextService;
-global.MetricsService = MetricsService;
-global.EventEmitter = EventEmitter;
--- frontend/src/lib/config/api.js
+++
@@ -1,5 +0,0 @@
-// frontend/src/lib/config/api.js               --> API Integration
-export const API_CONFIG = {
-    baseUrl: import.meta.env.PUBLIC_BACKEND_URL,
-    wsUrl: import.meta.env.PUBLIC_WS_URL
-};
