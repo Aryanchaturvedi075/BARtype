a23c3dd0029052ee9bfa8ea376440849d9a222bf
Aryan Chaturvedi <aryanchaturvedi075@gmail.com>
2024-12-03 22:54:34 -0400

    This fkn works!!!!

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,9 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-          target: 'http://localhost:3001',
-          changeOrigin: true
+          target: 'http://127.0.0.1:3001', // Use 127.0.0.1 instead of localhost
+          changeOrigin: true,
+          secure: false,
       }
     }
   },
